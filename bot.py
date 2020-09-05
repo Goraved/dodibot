d223c5855a2efbdc720695b710ff759e11d12d80
Goraved <roman.pobotin@gmail.com>
2020-09-05 12:40:36 +0300

    Add cancel next rehearsal function

--- bot.py
+++ bot.py
@@ -3,15 +3,16 @@ import os
 import telebot
 from flask import Flask, request
 
-from data import get_rehearsals, stickers
+from data import get_rehearsals, stickers, cancel_rehearsal
 
-options = {'Next': 'Who pays next?', 'Rehearsals': 'Rehearsals list', 'URL': 'Open site', 'Card': 'Card number'}
+options = {'Next': 'Who pays next?', 'Rehearsals': 'Rehearsals list', 'URL': 'Open site', 'Card': 'Card number',
+           'Cancel': 'Cancel next rehearsal'}
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row(options['Next'], options['Rehearsals'], options['URL'], options['Card'])
+keyboard1.row(options['Next'], options['Rehearsals'], options['URL'], options['Card'], options['Cancel'])
 
 
 @bot.message_handler(commands=['start'])
@@ -35,17 +36,24 @@ def send_text(message):
         bot.send_message(message.chat.id, get_rehearsals()[1], parse_mode="Markdown")
         bot.send_sticker(message.chat.id, stickers['list'])
     elif message.text.lower() == options['URL'].lower() or '/site' in message.text.lower():
-        bot.send_message(message.chat.id, f'Site URL - https://dodiki.herokuapp.com')
+        bot.send_message(message.chat.id, 'Site URL - https://dodiki.herokuapp.com')
     elif message.text.lower() == options['Card'].lower() or '/card' in message.text.lower():
-        bot.send_message(message.chat.id, f'Card number - 5375414106892499')
+        bot.send_message(message.chat.id, 'Card number - 5375414106892499')
+    elif message.text.lower() == options['Cancel'].lower() or '/cancel' in message.text.lower():
+        bot.send_message(message.chat.id, 'Canceling next rehearsal')
+        rehearsals = cancel_rehearsal()
+        bot.send_message(message.chat.id, 'Done! Next rehearsal:')
+        bot.send_message(message.chat.id, rehearsals[0], parse_mode="Markdown")
+        bot.send_sticker(message.chat.id, stickers[rehearsals[2]])
     else:
         bot.send_message(message.chat.id, 'Kurwa, I did not understand this command')
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBkl6pr4kVOGisB5LUX54w8USsN6hWAAL5AANWnb0KlWVuqyorGzYZBA')
 
 
-# Local
+# Uncomment to use local
 # bot.remove_webhook()
 # bot.polling(none_stop=True)
+
 
 # Heroku
 @server.route('/' + TOKEN, methods=['POST'])
@@ -54,6 +62,7 @@ def getMessage():
     return '!', 200
 
 
+# Comment to user local
 @server.route("/")
 def webhook():
     bot.remove_webhook()
