50ae87833deccda5bddae3b29e040b33ec41f15d
Goraved <roman.pobotin@gmail.com>
2021-01-02 15:33:36 +0200

    Fixes

--- bot.py
+++ bot.py
@@ -8,69 +8,70 @@ from flask import Flask, request
 
 from data import get_rehearsals, stickers, cancel_rehearsal, help_message, survey_question
 
-options = {'Next': 'Who pays next?', 'Rehearsals': 'Rehearsals list', 'URL': 'Open site', 'Card': 'Card number',
+OPTIONS = {'Next': 'Who pays next?', 'Rehearsals': 'Rehearsals list', 'URL': 'Open site', 'Card': 'Card number',
            'Cancel': 'Cancel next rehearsal', 'Survey': 'Survey', 'Help': 'Help'}
 TOKEN = os.getenv('TOKEN')
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-total_answers = 0
+BOT = telebot.TeleBot(TOKEN)
+SERVER = Flask(__name__)
+TOTAL_ANSWERS = 0
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row(options['Next'], options['Rehearsals'], options['URL'], options['Card'], options['Cancel'])
+KEYBOARD1 = telebot.types.ReplyKeyboardMarkup()
+KEYBOARD1.row(OPTIONS['Next'], OPTIONS['Rehearsals'], OPTIONS['URL'], OPTIONS['Card'], OPTIONS['Cancel'])
 
 
-@bot.message_handler(commands=['start'])
+@BOT.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_sticker(message.chat.id, stickers['sbt'])
-    bot.send_message(message.chat.id, 'Please choose an option', reply_markup=keyboard1)
+    BOT.send_sticker(message.chat.id, stickers['sbt'])
+    BOT.send_message(message.chat.id, 'Please choose an option', reply_markup=KEYBOARD1)
 
 
-@bot.message_handler(content_types=['sticker'])
+@BOT.message_handler(content_types=['sticker'])
 def get_sticker_id(sticker):
-    bot.send_message(sticker.chat.id, f'Sticker Id - {sticker.sticker.file_id}')
+    BOT.send_message(sticker.chat.id, f'Sticker Id - {sticker.sticker.file_id}')
 
 
-@bot.message_handler(content_types=['text'])
+@BOT.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == options['Next'].lower() or '/next' in message.text.lower():
+    if message.text.lower() == OPTIONS['Next'].lower() or '/next' in message.text.lower():
         rehearsals = get_rehearsals()
-        bot.send_message(message.chat.id, rehearsals[0], parse_mode="Markdown")
-        bot.send_sticker(message.chat.id, stickers[rehearsals[2]])
-    elif message.text.lower() == options['Rehearsals'].lower() or '/list' in message.text.lower():
-        bot.send_message(message.chat.id, get_rehearsals()[1], parse_mode="Markdown")
-        bot.send_sticker(message.chat.id, stickers['list'])
-    elif message.text.lower() == options['URL'].lower() or '/site' in message.text.lower():
-        bot.send_message(message.chat.id, 'Site URL - https://dodiki.herokuapp.com')
-    elif message.text.lower() == options['Card'].lower() or '/card' in message.text.lower():
-        bot.send_message(message.chat.id, 'Card number - 5375414106892499')
-    elif message.text.lower() == options['Cancel'].lower() or '/cancel' in message.text.lower():
-        bot.send_message(message.chat.id, 'Canceling next rehearsal')
+        BOT.send_message(message.chat.id, rehearsals[0], parse_mode="Markdown")
+        BOT.send_sticker(message.chat.id, stickers[rehearsals[2]])
+    elif message.text.lower() == OPTIONS['Rehearsals'].lower() or '/list' in message.text.lower():
+        BOT.send_message(message.chat.id, get_rehearsals()[1], parse_mode="Markdown")
+        BOT.send_sticker(message.chat.id, stickers['list'])
+    elif message.text.lower() == OPTIONS['URL'].lower() or '/site' in message.text.lower():
+        BOT.send_message(message.chat.id, 'Site URL - https://dodiki.herokuapp.com')
+    elif message.text.lower() == OPTIONS['Card'].lower() or '/card' in message.text.lower():
+        BOT.send_message(message.chat.id, 'Card number - 5375414106892499')
+    elif message.text.lower() == OPTIONS['Cancel'].lower() or '/cancel' in message.text.lower():
+        BOT.send_message(message.chat.id, 'Canceling next rehearsal')
         rehearsals = cancel_rehearsal()
-        bot.send_message(message.chat.id, 'Done! Next rehearsal:')
-        bot.send_message(message.chat.id, rehearsals[0], parse_mode="Markdown")
-        bot.send_sticker(message.chat.id, stickers[rehearsals[2]])
-    elif message.text.lower() == options['Help'].lower() or '/help' in message.text.lower():
-        bot.send_message(message.chat.id, help_message())
-    elif message.text.lower() == options['Survey'].lower() or '/survey' in message.text.lower():
-        run_survey()
+        BOT.send_message(message.chat.id, 'Done! Next rehearsal:')
+        BOT.send_message(message.chat.id, rehearsals[0], parse_mode="Markdown")
+        BOT.send_sticker(message.chat.id, stickers[rehearsals[2]])
+    elif message.text.lower() == OPTIONS['Help'].lower() or '/help' in message.text.lower():
+        BOT.send_message(message.chat.id, help_message())
+    elif message.text.lower() == OPTIONS['Survey'].lower() or '/survey' in message.text.lower():
+        run_survey(chat_id=message.chat.id)
     else:
-        bot.send_message(message.chat.id, 'Kurwa, I did not understand this command')
-        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBkl6pr4kVOGisB5LUX54w8USsN6hWAAL5AANWnb0KlWVuqyorGzYZBA')
+        BOT.send_message(message.chat.id, 'Kurwa, I did not understand this command')
+        BOT.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBkl6pr4kVOGisB5LUX54w8USsN6hWAAL5AANWnb0KlWVuqyorGzYZBA')
 
 
-def run_survey():
-    global total_answers
-    chat_id = int(os.getenv('CHAT_ID'))
-    admins = bot.get_chat_administrators(chat_id)
+def run_survey(chat_id: int = None):
+    global TOTAL_ANSWERS
+    if not chat_id:
+        chat_id = int(os.getenv('CHAT_ID'))
+    admins = BOT.get_chat_administrators(chat_id)
     users = ' '.join([f'[{admin.user.first_name}](tg://user?id={admin.user.id})' for admin in admins])
     question = survey_question()
 
-    if not total_answers:
-        bot.send_message(chat_id, users, parse_mode="Markdown")
-        bot.send_poll(chat_id, question, ('Yes', 'No'), is_anonymous=False, explanation_parse_mode="Markdown")
-        total_answers += 1
-    elif total_answers == 4:
-        bot.stop_poll(chat_id, question)
+    if not TOTAL_ANSWERS:
+        BOT.send_message(chat_id, users, parse_mode="Markdown")
+        BOT.send_poll(chat_id, question, ('Yes', 'No'), is_anonymous=False)
+        TOTAL_ANSWERS += 1
+    elif TOTAL_ANSWERS == 4:
+        BOT.stop_poll(chat_id, question)
 
 
 def schedule_checker():
@@ -81,8 +82,8 @@ def schedule_checker():
 
 def run_schedule():
     trigger_time = os.getenv('TRIGGER_TIME')
-    schedule.every().saturday.at(trigger_time).do(run_survey)
-    schedule.every().thursday.at(trigger_time).do(run_survey)
+    schedule.every().tuesday.at(trigger_time).do(run_survey)
+    schedule.every().friday.at(trigger_time).do(run_survey)
     # Spin up a thread to run the schedule check so it doesn't block your bot.
     # This will take the function schedule_checker which will check every second
     # to see if the scheduled job needs to be ran.
@@ -96,20 +97,20 @@ def run_schedule():
 
 
 # Heroku
-@server.route('/' + TOKEN, methods=['POST'])
+@SERVER.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    BOT.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return '!', 200
 
 
 # Comment to use local
-@server.route("/")
+@SERVER.route("/")
 def webhook():
-    bot.remove_webhook()
+    BOT.remove_webhook()
     run_schedule()
-    bot.set_webhook(url="https://dodibot.herokuapp.com/" + TOKEN)
+    BOT.set_webhook(url="https://dodibot.herokuapp.com/" + TOKEN)
     return "!", 200
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    SERVER.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
